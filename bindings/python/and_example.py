065cbcdfd93f77afc816805a7698340bed4def1a
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2025-11-07 11:22:05 -0300

    added and example

--- bindings/python/and_example.py
+++ bindings/python/and_example.py
@@ -3,13 +3,12 @@ from nnw import Model, Trainer, Activation, Optimizer, Loss, TrainerConfig
 
 def main():
     with Model(2) as model:
-        model.add_dense(4, Activation.RELU)
         model.add_dense(1, Activation.SIGMOID)
 
         cfg = TrainerConfig(3000, 4, True, 0.01)
         with Trainer(model, Optimizer.ADAMW, Loss.MSE, cfg) as trainer:
             X = np.array([[0,0],[0,1],[1,0],[1,1]], dtype=np.float32)
-            Y = np.array([[0],[1],[1],[0]], dtype=np.float32)
+            Y = np.array([[0],[0],[0],[1]], dtype=np.float32)
             trainer.fit(X, Y)
 
             preds = model.predict(X)
