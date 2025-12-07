f3fce174d3133339dedffc73f0ea4730cf8548ca
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2025-12-07 19:52:06 -0300

    added getters

--- include/rl/q-table.h
+++ include/rl/q-table.h
@@ -8,6 +8,10 @@ public:
 
   float get(size_t state, size_t action);
   void set(size_t state, size_t action, float value);
+  
+  size_t get_states_num() const { return states_num; }
+  size_t get_actions_num() const { return actions_num; }
+  const std::vector<float>& get_data() const { return m_data; }
 
 private:
   std::vector<float> m_data;
