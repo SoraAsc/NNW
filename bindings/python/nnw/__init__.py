ff01ecf74933fcb6e5d25873558ab1077f47e434
SoraAsc <57465032+SoraAsc@users.noreply.github.com>
2026-04-19 03:57:45 +0000

    Release v0.4.5
    Full release (binaries, changelog): https://github.com/SoraAsc/NNW/releases/tag/v0.4.5

--- bindings/python/nnw/__init__.py
+++
@@ -1,5 +0,0 @@
-from .core import Model, Trainer, NNError, TrainerConfig
-from .enums import Activation, Optimizer, Loss
-from . import _ffi
-
-__all__ = ["Model", "Trainer", "TrainerConfig", "NNError", "Activation", "Optimizer", "Loss", "_ffi"]
--- include/training/optimizer/optimizer.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "nn/model.h"
-
-class Optimizer {
-public:
-  virtual void step() = 0;
-  virtual void zero_grad() = 0;
-  virtual ~Optimizer() = default;
-};
