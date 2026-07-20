41a46d06ef445f163c641cfbf6e19faa9aac3d73
SoraAsc <57465032+SoraAsc@users.noreply.github.com>
2026-07-20 20:36:10 +0000

    Release v0.5.2
    Full release (binaries, changelog): https://github.com/SoraAsc/NNW/releases/tag/v0.5.2

--- include/nn/loss/mse_loss.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "nn/loss/loss.h"
-
-class MSELoss: public Loss {
-public:
-  float forward(const Tensor& predicted, const Tensor& target) override;
-  Tensor backward(const Tensor& predicted, const Tensor& target) override;
-  const char* name() const override { return "Mean Squared Error"; }
-};
